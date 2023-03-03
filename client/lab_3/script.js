b3b6b3630d817d1a6b8ce1935456946f9b6dfca5
Khaderbai <el.tobstaz9@gmail.com>
2023-03-03 17:47:46 -0500

    Finished Lab 3
    Added moveToPrevSlide if statement and reversed its effect on the carousel index system for the 'previous' button function.

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -19,11 +19,12 @@ const slidesArray = Array.from(slides);
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
-  slides.Array.forEach(slide => {
+  slidesArray.forEach((slide) => {
     slide.classList.remove('visible');
     slide.classList.add('hidden');
   });
 
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible');
 
 }
@@ -35,6 +36,12 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+  if(slidePosition === totalSlides - 1){
+    slidePosition = 0;
+  } else {
+    slidePosition += 1;
+  }
+
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -45,6 +52,12 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+    if(slidePosition === 0){
+      slidePosition = totalSlides - 1;
+    } else {
+      slidePosition -= 1;
+    }
+
   updateSlidePosition();
 }
 
@@ -60,3 +73,9 @@ document.querySelector('.next') // Get the appropriate element (<button class="n
 
 // Paying close attention to the above queryselector, write one that fires
 // when you want a "prev" slide
+
+document.querySelector('.prev') // Get the appropriate element (<button class="next">)
+  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
+    console.log('clicked prev'); // let's tell the client console we made it to this point in the script
+    moveToPrevSlide(); // call the function above to handle this
+  });
