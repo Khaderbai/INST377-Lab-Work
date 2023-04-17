7caa4278fb61164e304bb5cfb6f4a77e63d6a893
Khaderbai <el.tobstaz9@gmail.com>
2023-04-16 21:21:17 -0400

    Finished Lab 8
    Working Lab 8 completed.

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -41,11 +41,36 @@ function cutRestaurantList(list) {
   })
 }
   
+function initMap() {
+  const carto = L.map('map').setView([38.98, -76.93], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+}
 
+function markerPlace(array, map) {
+  console.log('array for markers', array);
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log('markerPlace', item);
+    const {coordinates} = item.geocoded_column_1;
+
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  })
+}
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
   const loadDataButton = document.querySelector('#data_load');
+  const clearDataButton = document.querySelector('#data_clear');
   const generateListButton = document.querySelector('#generate');
   const textField = document.querySelector('#resto');
 
@@ -53,14 +78,14 @@ async function mainEvent() { // the async keyword means we can make API requests
   loadAnimation.style.display = 'none';
   generateListButton.classList.add('hidden');
 
-  // Add a querySelector that targets your filter button here
+  const carto = initMap();
   
   const storedData = localStorage.getItem('storedData');
-  const parsedData = JSON.parse(storedData);
-
-  if (parsedData.length > 0) {
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0) {
     generateListButton.classList.remove("hidden");
   }
+
   let currentList = []; // this is "scoped" to the main event function
   
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
@@ -75,7 +100,10 @@ async function mainEvent() { // the async keyword means we can make API requests
     // This changes the response from the GET into data we can use - an "object"
     const storedList = await results.json();
     localStorage.setItem('storedData', JSON.stringify(storedList));
-
+    parsedData = storedList;
+    if (parsedData?.length > 0) {
+      generateListButton.classList.remove("hidden");
+    }
     loadAnimation.style.display = 'none';
     // console.table(storedList);
 
@@ -86,9 +114,10 @@ async function mainEvent() { // the async keyword means we can make API requests
 
   generateListButton.addEventListener('click', (event) => {
     console.log('generate new list');
-    currentList = cutRestaurantList(storedList);
+    currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   })
 
   textField.addEventListener('input', (event) => {
@@ -96,6 +125,13 @@ async function mainEvent() { // the async keyword means we can make API requests
       const newList = filterList(currentList, event.target.value);
       console.log(newList);
       injectHTML(newList);
+      markerPlace(newList, carto);
+  })
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear();
+    console.log('localStorage Check', localStorage.getItem("storedData"));
   })
 }
 
